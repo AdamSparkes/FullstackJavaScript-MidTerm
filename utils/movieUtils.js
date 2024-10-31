302a2c1dd2353dec8f34a973e4dcf22a5523cf21
Elbaton1 <Michael.o'brien@keyin.com>
2024-10-31 00:04:03 -0230

    update movie utils with  getTopRatedMovies and selectRandomMovieId by mike

--- utils/movieUtils.js
+++ utils/movieUtils.js
@@ -1,3 +1,5 @@
+// utils/movieUtils.js
+
 const { Movies, Genres } = require("./data");
 
 /**
@@ -7,9 +9,10 @@ const { Movies, Genres } = require("./data");
  * @returns {Array.<Movies>} - An array of movies matching the genre
  */
 function getMoviesByGenre(genre, x) {
-
-    const filteredMovies = Movies.filter(movie => movie.genre === genre);
-    return filteredMovies.slice(0, x);e
+  // Filter movies by genre
+  const filteredMovies = Movies.filter((movie) => movie.genre === genre);
+  // Get first 'x' movies
+  return filteredMovies.slice(0, x);
 }
 
 /**
@@ -18,7 +21,12 @@ function getMoviesByGenre(genre, x) {
  * @returns {Array.<Movies>} - An array of top-rated movies
  */
 function getTopRatedMovies(x) {
-    // Implementation here
+  // Make a copy of movies so we don't change the original
+  const moviesCopy = [...Movies];
+  // Sort movies by rating from high to low
+  moviesCopy.sort((a, b) => b.rating - a.rating);
+  // Get the first 'x' movies
+  return moviesCopy.slice(0, x);
 }
 
 /**
@@ -27,9 +35,10 @@ function getTopRatedMovies(x) {
  * @returns {Movies} - The movie object
  */
 function getMovieDetailsById(id) {
-    
-    const movie = Movies.find(movie => movie.id === parseInt(id, 10));
-    return movie || null;
+  // Find the movie with the given ID
+  const movie = Movies.find((movie) => movie.id === parseInt(id, 10));
+  // Return the movie or null if not found
+  return movie || null;
 }
 
 /**
@@ -37,13 +46,18 @@ function getMovieDetailsById(id) {
  * @returns {number} - A random movie ID
  */
 function selectRandomMovieId() {
-    // Implementation here
+  // Get a random index
+  const randomIndex = Math.floor(Math.random() * Movies.length);
+  // Get the movie at that index
+  const movie = Movies[randomIndex];
+  // Return the movie ID
+  return movie.id;
 }
 
 // Export the functions to be used in other modules
 module.exports = {
-    getMoviesByGenre,
-    getTopRatedMovies,
-    getMovieDetailsById,
-    selectRandomMovieId,
+  getMoviesByGenre,
+  getTopRatedMovies,
+  getMovieDetailsById,
+  selectRandomMovieId,
 };
